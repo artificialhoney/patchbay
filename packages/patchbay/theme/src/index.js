7f0b0848d7ddd1129ae9290891bcdddcf9c61811
Sebastian Krüger <sk@honeymachine.io>
2025-05-04 13:49:01 +0200

    feat: patchbay theme (wip)

---
+++ packages/patchbay/theme/src/index.js
@@ -0,0 +1,12 @@
+export default {
+  id: "@patchbay/theme",
+  name: "Patchbay Theme",
+  appearance: "dark",
+  rules: {
+    navigation: {
+      modules: {
+        background: "#07f78c",
+      },
+    },
+  },
+};
